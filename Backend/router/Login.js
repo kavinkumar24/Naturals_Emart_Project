ece8901bc253cc56dc20a8ed01e4980cbaf24a55
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2024-10-11 18:25:46 +0530

    Update Login.js

--- Backend/router/Login.js
+++ Backend/router/Login.js
@@ -1,39 +1,57 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-
-const jwt = require('jsonwebtoken'); // Import jsonwebtoken
-const User = require('../config/Schema'); // Adjust path as necessary
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const UserSession = require("../config/model/UserSession");
+const jwt = require("jsonwebtoken"); // Import jsonwebtoken
+const User = require("../config/Schema"); // Adjust path as necessary
 const router = express.Router();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { phone, password } = req.body;
   try {
     // Find the user by phone number
     const user = await User.findOne({ phone });
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     // Check password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ message: 'Invalid credentials' });
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
     // Generate JWT token
-    const token = jwt.sign({ userId: user._id, phone: user.phone }, JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign(
+      { userId: user._id, phone: user.phone },
+      JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    // Create or update user session
+    await UserSession.findOneAndUpdate(
+      { userId: user._id },
+      { isLoggedIn: true, lastLogin: Date.now() },
+      { upsert: true, new: true }
+    );
 
     // Send the token and user data in response
-    res.status(200).json({ 
-      message: 'Login successful', 
-      token, 
-      user: { name: user.name, phone: user.phone, thaluka:user.thaluka, district: user.district, addres:user.address, place:user.place} 
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        name: user.name,
+        phone: user.phone,
+        thaluka: user.thaluka,
+        district: user.district,
+        address: user.address,
+        place: user.place,
+      },
     });
   } catch (error) {
-    console.error('Error during login:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error during login:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
